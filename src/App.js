1aa7f8230fe1277825107b80cfebc1e9f3944168
Arif Hossain <arif.hossain@se.experis.com>
2023-01-19 11:07:05 +0100

    install router dom to the project and make some empty component and route them throw parent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from 'react-router-dom';
+import Login from "./views/Login";
+import Orders from "./views/Orders";
+import Profile from "./views/Profile";
+
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+    return (
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Login/>}/>
+              <Route path="/orders" element={<Orders/>}/>
+              <Route path="/profile" element={<Profile/>}/>
+            </Routes>
+          </div>
+        </BrowserRouter>
+
+    );
 }
 
 export default App;
---
+++ src/views/Orders.jsx
@@ -0,0 +1,7 @@
+const Orders = () => {
+    return (
+        <h1>Orders</h1>
+    )
+}
+
+export default Orders;
