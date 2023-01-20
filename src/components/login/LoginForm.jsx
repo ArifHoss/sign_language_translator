8a547aa32cea5d09a9dbee6c6b5634a83d2eb60e
Arif Hossain <arif.hossain@se.experis.com>
2023-01-20 08:45:09 +0100

    added some comments and change minLength to 3

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -2,12 +2,23 @@ import {useForm} from 'react-hook-form'
 
 const userNameConfig = {
     required: true,
-    minLength: 2
+    minLength: 3
 }
 
 const LoginForm = () => {
 
     const {register, handleSubmit, formState: {errors}} = useForm()
+    /*
+    * This is JavaScript code that is using the `useForm` hook from a library called react-hook-form.
+    * `register´ is a function that is used to register input elements (e.g. text fields, checkboxes, etc.)
+      with the form so that their values can be accessed and validated when the form is submitted.
+
+    * `handleSubmit` is a function that is used to handle the form submission event.
+    * It typically takes a callback function as its argument, which is executed when the form is submitted.
+    * `formState.errors` is an object that contains any errors that have been detected in the form's input elements.
+    * The errors variable is destructured from the formState object returned by the useForm hook.
+    * This code is likely to be used within a functional component in React, and it is mainly used for form validation and submission.
+    * */
 
     const onSubmit = (data) => {
         console.log(data);
@@ -27,6 +38,18 @@ const LoginForm = () => {
             return <span>Minimum length is required</span>;
         }
     })()
+
+    /*
+    * The () at the end of the function definition is used to immediately invoke the function.
+
+    * This means that the function is executed as soon as it is defined, and the return value of the function is assigned to the variable handleError.
+
+    * It's equivalent to calling the function with handleError().
+
+    * This pattern is useful when you want to create a function that only needs to be executed once, and you want to store its return value in a variable for later use.
+
+    * In this case, the handleError function is invoked only once when the component first renders and the returned value is stored in the variable handleError and that value is used later in the JSX.
+    * */
 
 
     return (
